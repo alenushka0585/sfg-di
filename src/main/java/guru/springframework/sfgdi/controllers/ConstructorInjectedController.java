15c6cdcccdf6dc34b86fabbf0a48830bb61b392e
Yelena Kaloshina <yelena_kaloshina@epam.com>
2020-10-25 15:17:19 +0600

    initial commit

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+    private final ConstructorGreetingService greetingService;
+
+
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") ConstructorGreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
